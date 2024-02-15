dd3085a6dc8965228d835dd002c70b77efc741ed
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-02-15 15:21:52 +0000

    from codespaces

---
+++ docker/mage/task.sh
@@ -0,0 +1,3 @@
+sudo cp ./docker/mage/batch_pipeline/exporter/* ./docker/mage/ashraf-magic/data_exporters/
+
+sudo cp ./docker/mage/batch_pipeline/ingestion/* ./docker/mage/ashraf-magic/data_loaders/
