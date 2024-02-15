7b46e9c7f6bc34921280bb11c2a5339548828beb
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-02-15 17:18:32 +0000

    update from codespaces

--- docker/mage/task.sh
+++ docker/mage/task.sh
@@ -12,4 +12,6 @@ sudo cp ./docker/mage/batch_pipeline/triggers.yaml ./docker/mage/ashraf-magic/pi
 sudo cp ./docker/mage/batch_pipeline/io_config.yaml ./docker/mage/ashraf-magic/
 
 
-curl -X POST http://127.0.0.1:6789/api/pipeline_schedules/4/pipeline_runs/515ea1cd64dc4cea8ec0ab0226dcd356 
+curl -X POST http://127.0.0.1:6789/api/pipeline_schedules/1/pipeline_runs/beaba82164454f1aa4641eb0b37de207
+
+
