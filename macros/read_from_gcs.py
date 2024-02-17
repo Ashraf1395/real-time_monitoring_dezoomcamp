f011a743401a302c346b555dea94dbd741620cae
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-02-17 09:17:01 +0000

    update from codespace

---
+++ macros/read_from_gcs.py
@@ -0,0 +1,9 @@
+from google.cloud import storage
+import pandas as pd
+
+def read_csv_from_gcs(bucket_name, file_path):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_path)
+    data = blob.download_as_string()
+    return pd.read_csv(io.BytesIO(data))
--- models/core/dim_module_dbt_model.sql
+++ models/core/dim_module_dbt_model.sql
@@ -1,1 +1,14 @@
-select * from {{ copy('gs://de-zoomcamp-project-data/historical_data/time_spent.parquet') }}
+
+
+{% set gcs_data = read_csv_from_gcs('de-zoomcamp-project-data', '/historical_data/time_spent.parquet') %}
+
+{{ config(
+    materialized='table',
+    schema='de_data',
+    unique_key='email'
+) }}
+
+SELECT *
+FROM {{ gcs_data }}
+
+-- select * from {{ copy('gs://de-zoomcamp-project-data/historical_data/time_spent.parquet') }}
