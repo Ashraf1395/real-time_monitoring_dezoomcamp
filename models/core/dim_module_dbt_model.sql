5fa99917d468b796f35ef334d6217f68a2b19c4b
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-02-17 09:55:30 +0000

    update from codespace

--- models/core/dim_module_dbt_model.sql
+++ models/core/dim_module_dbt_model.sql
@@ -1,14 +1,4 @@
-
-
-{% set gcs_data = read_from_gcs('de-zoomcamp-project-data', '/historical_data/time_spent.parquet') %}
-
-{{ config(
-    materialized='table',
-    schema='de_data',
-    unique_key='email'
-) }}
-
-SELECT *
-FROM {{ gcs_data }}
+select *
+from {{ source('gs://my_bucket', 'my_file.csv') }}
 
 -- select * from {{ copy('gs://de-zoomcamp-project-data/historical_data/time_spent.parquet') }}
