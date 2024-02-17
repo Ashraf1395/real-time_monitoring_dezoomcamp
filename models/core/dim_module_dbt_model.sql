3b63eb7b883ae1463c21a193bec1f5b4ea9dd488
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-02-17 09:58:26 +0000

    update from codespace

--- models/core/dim_module_dbt_model.sql
+++ models/core/dim_module_dbt_model.sql
@@ -1,4 +1,4 @@
 select *
-from {{ source('gs://de-zoomcamp-project-data', 'historical_data/time_spent.parquet') }}
+from {{ source('gs://de-zoomcamp-project-data/', 'historical_data/time_spent.parquet') }}
 
 -- select * from {{ copy('gs://de-zoomcamp-project-data/historical_data/time_spent.parquet') }}
