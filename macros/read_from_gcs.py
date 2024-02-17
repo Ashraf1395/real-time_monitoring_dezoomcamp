eb8c7872126cd8ecbb6a4a328f693b3920eb244e
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-02-17 09:35:30 +0000

    update from codespace

--- macros/read_from_gcs.py
+++ macros/read_from_gcs.py
@@ -1,9 +1,13 @@
+
 from google.cloud import storage
 import pandas as pd
+import pyarrow.parquet as pq
 
 def read_from_gcs(bucket_name, file_path):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(file_path)
     data = blob.download_as_string()
-    return pd.read_csv(io.BytesIO(data))
+
+    # Read Parquet file using pyarrow
+    return pq.read_table(io.BytesIO(data)).to_pandas()
